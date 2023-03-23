1cc4f56a9f20fb2332cc9c1e579f790a728f4959
daneeskripter <imdanee@seznam.cz>
2023-03-23 16:45:36 +0100

    everything added to help command

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ new Command({
         const embed = new Discord.EmbedBuilder()
         .setColor("#5D3FD3")
         .setTitle("Help")
-        .setDescription(`**👤 User**\n**/user** - Get info about user\n**/users** - Get list of all users\n**/register** - Register new account\n**/edituser** - Edit a user account\n**/deluser** - Delete a user account\n**/userrole** - Change user's role\n**/verifyuser** - Send verification email to user\n\n**💿 Files**\n**/files** - Show user's files\n**/renfile** - Rename a file\n**/delfile** - Delete a file\n**/share** - Share a file\n**/disableshare** - Set file as not shared\n\n**📳 Moderation**\n**/clear** - Clear messages in channel`)
+        .setDescription(`**👤 User**\n**/user** - Get info about user\n**/users** - Get list of all users\n**/register** - Register new account\n**/edituser** - Edit a user account\n**/deluser** - Delete a user account\n**/userrole** - Change user's role\n**/verifyuser** - Send verification email to user\n\n**💿 Files**\n**/files** - Show user's files\n**/renfile** - Rename a file\n**/delfile** - Delete a file\n**/share** - Share a file\n**/disableshare** - Set file as not shared\n\n**🎫 Roles**\n**/roles** - Get list of roles\n**/addrole** - Create new role\n**/delrole** - Delete role\n**/editrole** - Edit role\n\n**☁️ Dash**\n**/info** - Get info about bot and dash\n\n**📳 Moderation**\n**/clear** - Clear messages in channel`)
         ctx.reply({ embeds: [embed], ephemeral: true})
 	}
 });
