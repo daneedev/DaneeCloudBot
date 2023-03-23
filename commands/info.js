bf6cb083a7e82845143c299a92161e54219d4b7d
daneeskripter <imdanee@seznam.cz>
2023-03-23 16:45:14 +0100

    info command

---
+++ commands/info.js
@@ -0,0 +1,37 @@
+const { Command, CommandType } = require('gcommands');
+const Discord = require("discord.js")
+const request = require("request")
+const axios = require("axios")
+const packages = require("../package.json")
+
+new Command({
+	name: 'info',
+	description: 'Get info about bot and dash',
+	type: [CommandType.SLASH],
+	run: async (ctx) => {
+        const dash = await axios.get(process.env.cloud_url + `/api/dash/`, {
+            headers: { "API-Key" : process.env.api_key},
+            validateStatus: function (status) {
+                return status < 500; // Resolve only if the status code is less than 500
+            }
+        })
+        request.get("https://version.daneeskripter.dev/daneecloud/version.txt", function (error, response, body) {
+        request.get("https://version.daneeskripter.dev/daneecloudbot/version.txt", function (error2, response2, body2) {
+        const embed = new Discord.EmbedBuilder()
+        .setColor("#5D3FD3")
+        .setTitle("Info")
+        .addFields(
+            {name: "Dash CPU Usage", value: dash.data.cpuUsage, inline: true},
+            {name: "Dash RAM Usage", value: dash.data.ramUsage, inline: true},
+            {name: "Dash Uptime", value: dash.data.uptime, inline: true},
+            {name: "Dash Current version", value: dash.data.version, inline: true},
+            {name: "Dash Latest version", value: `v${body}`, inline: true},
+            {name: "Bot Current version", value: `v${packages.version}`, inline: true},
+            {name: "Bot Latest version", value: `v${body2}`, inline: true},
+            {name: "Developer", value: dash.data.developer, inline: true},
+        )
+        ctx.reply({ embeds: [embed], ephemeral: true})
+    })
+        })
+	}
+});
