1b83df1925e5586c8424a4f2b0916302e8a77799
daneeskripter <imdanee@seznam.cz>
2023-03-23 15:51:07 +0100

    delrole command

---
+++ commands/delrole.js
@@ -0,0 +1,54 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const Discord = require("discord.js")
+const axios = require("axios")
+
+new Command({
+	name: 'delrole',
+	description: 'Delete role',
+	type: [CommandType.SLASH],
+    arguments: [
+        new Argument({
+            name: "name",
+            description: "Name of role",
+            type: ArgumentType.STRING,
+            required: true
+        })
+    ],
+	run: async (ctx) => {
+        const rolename = ctx.arguments.getString("name")
+        if (ctx.member.permissions.has(process.env.admin_perm)) {
+            const delrole = await axios.post(process.env.cloud_url + `/api/role/delete?name=${rolename}`, {}, {
+                headers: { "API-Key" : process.env.api_key},
+                validateStatus: function (status) {
+                    return status < 500; // Resolve only if the status code is less than 500
+                }
+            })
+            if (delrole.status == 200) {
+            const embed = new Discord.EmbedBuilder()
+                .setTitle(`Role ${rolename} has been deleted`)
+                .setColor("#5D3FD3")
+            ctx.reply({ embeds: [embed], ephemeral: true})
+            } else if (delrole.status == 404) {
+                    const err = new Discord.EmbedBuilder()
+                    .setTitle("Role not found")
+                    .setColor("#FF9494")
+                ctx.reply({ embeds: [err], ephemeral: true})
+           } else if (delrole.status == 401) {
+            const err = new Discord.EmbedBuilder()
+            .setTitle("API: Invalid API Key")
+            .setColor("#FF9494")
+        ctx.reply({ embeds: [err], ephemeral: true})
+           } else if (delrole.status == 409) {
+            const err = new Discord.EmbedBuilder()
+            .setTitle(`You can't delete ${rolename} role`)
+            .setColor("#FF9494")
+        ctx.reply({ embeds: [err], ephemeral: true})
+           }
+        } else {
+            const err = new Discord.EmbedBuilder()
+                .setTitle("You have no permission to do that.")
+                .setColor("#FF9494")
+            ctx.reply({ embeds: [err], ephemeral: true})
+        }
+	}
+});
