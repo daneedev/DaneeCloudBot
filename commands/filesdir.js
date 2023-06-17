2e914966571fbaef686b99348ef37bec5ae14fff
daneeskripter <imdanee@seznam.cz>
2023-06-17 19:43:12 +0200

    get files from folder command

---
+++ commands/filesdir.js
@@ -0,0 +1,61 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const Discord = require("discord.js")
+const {Cloud} = require("daneecloud-api")
+const cloud = Cloud({
+	cloudUrl: process.env.cloud_url,
+	apiKey: process.env.api_key
+})
+
+new Command({
+	name: 'folderfiles',
+	description: 'Get list of files in folder',
+	type: [CommandType.SLASH],
+    arguments: [
+        new Argument({
+            name: "username",
+            description: "Username of user",
+            type: ArgumentType.STRING,
+            required: true
+        }),
+        new Argument({
+            name: "folder",
+            description: "Name of folder",
+            type: ArgumentType.STRING,
+            required: true
+        })
+    ],
+	run: async (ctx) => {
+        const username = ctx.arguments.getString("username")
+        const folder = ctx.arguments.getString("folder")
+        const files = await cloud.getFilesFromFolder(username, folder)
+        if (ctx.member.permissions.has(process.env.admin_perm)) {
+            if (files.code == 200) {
+            let allfiles = ""
+            for (let file of files.data.files) {
+                allfiles = allfiles + `\n**${file}**`
+            }
+            const embed = new Discord.EmbedBuilder()
+                .setTitle(`${username}'s files in /${folder}`)
+                .setColor("#5D3FD3")
+                .setDescription(allfiles)
+            ctx.reply({ embeds: [embed], ephemeral: true})
+        } else if (files.code == 404) {
+                const err = new Discord.EmbedBuilder()
+                .setTitle(files.data)
+                .setColor("#FF9494")
+            ctx.reply({ embeds: [err], ephemeral: true})
+        
+        } else if (files.code == 401) {
+            const err = new Discord.EmbedBuilder()
+            .setTitle("API: Invalid API Key")
+            .setColor("#FF9494")
+        ctx.reply({ embeds: [err], ephemeral: true})
+        }
+        } else {
+            const err = new Discord.EmbedBuilder()
+                .setTitle("You have no permission to do that.")
+                .setColor("#FF9494")
+            ctx.reply({ embeds: [err], ephemeral: true})
+        }
+	}
+});
