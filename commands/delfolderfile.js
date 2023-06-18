4947088b049f3d01595e3d24bebc7f5928972e87
daneeskripter <imdanee@seznam.cz>
2023-06-18 11:04:08 +0200

    delete file in folder command

---
+++ commands/delfolderfile.js
@@ -0,0 +1,69 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const Discord = require("discord.js")
+const {Cloud} = require("daneecloud-api")
+const cloud = Cloud({
+	cloudUrl: process.env.cloud_url,
+	apiKey: process.env.api_key
+})
+
+new Command({
+	name: 'delfolderfile',
+	description: 'Delete a file in folder',
+	type: [CommandType.SLASH],
+    arguments: [
+        new Argument({
+            name: "username",
+            description: "Username of user",
+            type: ArgumentType.STRING,
+            required: true
+        }),
+        new Argument({
+            name: "filename",
+            description: "Name of file",
+            type: ArgumentType.STRING,
+            required: true
+        }),
+        new Argument({
+            name: "foldername",
+            description: "Name of folder",
+            type: ArgumentType.STRING,
+            required: true
+        })
+    ],
+	run: async (ctx) => {
+		const username = ctx.arguments.getString("username")
+        const file = ctx.arguments.getString("filename")
+        const folder = ctx.arguments.getString("foldername")
+        if (ctx.member.permissions.has(process.env.admin_perm)) {
+            const delfile = await cloud.deleteFileFolder(username, folder, file)
+            if (delfile.code == 200) {
+            const embed = new Discord.EmbedBuilder()
+                .setTitle(`File ${file} in /${folder} has been deleted`)
+                .setColor("#5D3FD3")
+            ctx.reply({ embeds: [embed], ephemeral: true})
+            } else if (delfile.code == 404) {
+                if (delfile.data == "File not found") {
+                    const err = new Discord.EmbedBuilder()
+                    .setTitle("File not found")
+                    .setColor("#FF9494")
+                ctx.reply({ embeds: [err], ephemeral: true})
+                } else {
+                    const err = new Discord.EmbedBuilder()
+                    .setTitle(delfile.data)
+                    .setColor("#FF9494")
+                ctx.reply({ embeds: [err], ephemeral: true})
+                }
+           } else if (delfile.code == 401) {
+            const err = new Discord.EmbedBuilder()
+            .setTitle("API: Invalid API Key")
+            .setColor("#FF9494")
+        ctx.reply({ embeds: [err], ephemeral: true})
+           }
+        } else {
+            const err = new Discord.EmbedBuilder()
+                .setTitle("You have no permission to do that.")
+                .setColor("#FF9494")
+            ctx.reply({ embeds: [err], ephemeral: true})
+        }
+	}
+});
